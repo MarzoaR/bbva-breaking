625377aa9a0b0f737491e0205355d21623e61604
Roberto Marzoa <roberto.marzoa@insoftar.com>
2022-09-20 14:55:42 +0100

    Se crea el alert con sweetalert2 y se muestra la data de episodios por personaje

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -4,6 +4,8 @@ import { EpisodeService } from 'src/app/services/episode.service';
 import { CharacterResponse } from 'src/app/interfaces/character.interface';
 import { EpisodeResponse } from 'src/app/interfaces/episode.interface';
 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -29,11 +31,25 @@ export class CardComponent implements OnInit {
     });
   }
 
-  alert( name: string): void {
-    alert( name );
+  getInfoEpisodes( name: string): void {
+    const episodesByCharacter: EpisodeResponse[] = this.episode.getEpisodesByCharacter( name, this.episodes );
     console.log(this.episode.getEpisodesByCharacter(name, this.episodes));
 
     console.log(this.episodes)
+
+    let text: string = '';
+
+    // for (const episode of episodesByCharacter) {
+    //   text += `<p>Season: ${episode.season} - Episode: ${episode.episode}</p><p>Title: ${episode.title}</p>`;
+    // }
+
+    episodesByCharacter.forEach(episode => text += `<p>Season: ${episode.season} - Episode: ${episode.episode}</p><p>Title: ${episode.title}</p>`);
+
+    Swal.fire({
+      title: name,
+      html: `${text}`,
+    })
+
   }
 
 }
