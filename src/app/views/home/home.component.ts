230e5ffcb5ae9d3187ad30ab14b1e8915f669d9f
Roberto Marzoa <roberto.marzoa@insoftar.com>
2022-09-20 09:11:04 +0100

    Se modifica el html extrayendo la maquetación a un componente adicional, se crean los estilos y los métodos del componente home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CharacterResponse } from 'src/app/interfaces/character.interface';
+import { ActorService } from 'src/app/services/actor.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  characters: CharacterResponse[] = [];
+
+  constructor(  private actors:ActorService ) { }
 
   ngOnInit(): void {
+    this.getCharacters();
+  }
+
+  getCharacters() {
+    this.actors.getCharacters()
+    .subscribe( (resp: CharacterResponse) => {
+      this.getCharactersBySeason(resp);
+    } );
+  }
+
+  getCharactersBySeason( characters: CharacterResponse) {
+    this.getCharacterOrder( this.actors.getCharactersBySeason(characters) );
+  }
+
+  getCharacterOrder( characters: CharacterResponse ) {
+    this.getCharaterOrderReverse( this.actors.getCharacterOrder(characters) );
+  }
+
+  getCharaterOrderReverse( characters: CharacterResponse ) {
+    this.characters = this.actors.getCharaterOrderReverse( characters );
   }
 
 }
