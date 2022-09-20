43030740652629b32ab1971d37f6213f2f12c798
Roberto Marzoa <roberto.marzoa@insoftar.com>
2022-09-20 09:06:59 +0100

    Se crea interfaz character para las peticiones en el servicio actor y los métodos necesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { CardComponent } from './components/card/card.component';
@@ -14,7 +15,8 @@ import { CardComponent } from './components/card/card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/actor.service.ts
+++ src/app/services/actor.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+import { CharacterResponse } from '../interfaces/character.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActorService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  getCharacters(): Observable<CharacterResponse>{
+    return this.http.get<CharacterResponse>('/assets/data/character.json').pipe( delay(740) );
+  }
+
+  getCharactersBySeason( characters: any): any {
+    return characters.filter( (character: any) => character.appearance.includes(3) && character.appearance.includes(5) );
+  }
+
+  getCharacterOrder( characters: any ): any {
+    return characters.sort( (a: any, b: any) => {
+      if ( a.name > b.name ) {
+        return 1;
+      }
+      if ( a.name < b.name ) {
+        return -1;
+      }
+      return 0;
+      // a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
+    });
+  }
+
+  getCharaterOrderReverse( characters: any ): any {
+    return characters.reverse();
+  }
+
 }
+
